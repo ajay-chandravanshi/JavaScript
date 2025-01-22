878ef222827ab3855400fef16c6009c000474bda
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-01-22 14:26:13 +0530

    new code

---
+++ 8.Conditional Statement(if,if esle,else)/3.If Else-If Statement/6.time code using if,else if, else.js
@@ -0,0 +1,18 @@
+let time=promt("Enter the time");
+
+    if (time < 12) 
+    {
+    cout << "Good Morning.";
+    } 
+    else if (time < 16) 
+    {
+    cout << "Good Afternoon.";
+    } 
+    else if(time < 20)
+    {
+     cout << "Good Evening.";
+    }
+    else
+    {
+     cout << "Good Night.";
+    }
