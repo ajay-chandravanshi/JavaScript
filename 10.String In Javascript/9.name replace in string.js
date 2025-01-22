bdf2432ee6751f2995373651d905245af9cfde27
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-01-22 14:58:43 +0530

    uudate

--- 10.String In Javascript/9.name replace in string.js
+++ 10.String In Javascript/9.name replace in string.js
@@ -1,2 +1,3 @@
-let str7="Hello sir my name is ajay chandravanshi"
+let str7="Hello is sir my name is ajay chandravanshi"
 console.log(str7.replace("ajay","Aniket"))
+console.log(str7.replaceAll("is",","))
